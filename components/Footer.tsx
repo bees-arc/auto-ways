55823b42b133020eda52b4b7b40e25e1e1659241
Benara Gunaweera <benara@misynergy.com>
2026-09-21 17:55:19 +0530

    feat: add Footer component and Navbar module stylesheet

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -52,6 +52,7 @@ export default function Footer() {
             <li><Link href="/about" className={styles.footerLink}>About</Link></li>
             <li><Link href="/services" className={styles.footerLink}>Services</Link></li>
             <li><Link href="/specifications" className={styles.footerLink}>Specifications</Link></li>
+            <li><Link href="/articles" className={styles.footerLink}>Articles</Link></li>
             <li><Link href="/contact" className={styles.footerLink}>Contact</Link></li>
           </ul>
         </div>
